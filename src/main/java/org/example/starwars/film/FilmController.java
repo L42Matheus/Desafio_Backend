875c8eec6446a35dd7d1a7202d7f7a21aca178ed
matheus <lmatheus.tome@gmail.com>
2024-03-12 20:58:10 -0300

    add method to retrieve all films using filmService.getAllFilms()

--- src/main/java/org/example/starwars/film/FilmController.java
+++ src/main/java/org/example/starwars/film/FilmController.java
@@ -1,8 +1,11 @@
 package org.example.starwars.film;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("swapi/api")
@@ -11,8 +14,8 @@ public class FilmController {
     @Autowired
     FilmService filmService;
 
-
-
-
-
+    @GetMapping("/films")
+    public List<Film> getAllFilms(){
+        return filmService.getAllFilms();
+    }
 }
