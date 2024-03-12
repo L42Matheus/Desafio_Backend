45d745d94ba093d3eaab51499f5367d08d49d04e
matheus <lmatheus.tome@gmail.com>
2024-03-12 20:54:33 -0300

    set the base path for all controller methods to "/swapi/api"

--- src/main/java/org/example/starwars/film/FilmController.java
+++ src/main/java/org/example/starwars/film/FilmController.java
@@ -1,8 +1,10 @@
 package org.example.starwars.film;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("swapi/api")
 public class FilmController {
 
 
