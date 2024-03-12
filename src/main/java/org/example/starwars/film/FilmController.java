d74356cee3fece704b627ad14a533d3d4cdd153d
matheus <lmatheus.tome@gmail.com>
2024-03-12 20:55:18 -0300

    add inject FilmService instance

--- src/main/java/org/example/starwars/film/FilmController.java
+++ src/main/java/org/example/starwars/film/FilmController.java
@@ -1,11 +1,16 @@
 package org.example.starwars.film;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("swapi/api")
 public class FilmController {
+
+    @Autowired
+    FilmService filmService;
+
 
 
 
