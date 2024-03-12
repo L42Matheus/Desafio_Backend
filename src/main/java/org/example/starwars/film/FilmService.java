a1b1cb59f2f22302178bd5628d1e5e4cd7481d0f
matheus <lmatheus.tome@gmail.com>
2024-03-12 20:38:22 -0300

    check for successful response status (HTTP 200)

--- src/main/java/org/example/starwars/film/FilmService.java
+++ src/main/java/org/example/starwars/film/FilmService.java
@@ -1,10 +1,12 @@
 package org.example.starwars.film;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Service
@@ -17,5 +19,18 @@ public class FilmService {
         String url = "https://swapi.info/api/films";
 
         ResponseEntity<Film[]> response = restTemplate.getForEntity(url, Film[].class);
+
+        if (response.getStatusCode() == HttpStatus.OK) {
+            Film[] films = response.getBody();
+
+            if (films != null && films.length > 0) {
+
+                return Arrays.asList(films);
+            }
+
+            throw new RuntimeException("Lista vazia ou nula");
+
+        }
+
     }
 }
