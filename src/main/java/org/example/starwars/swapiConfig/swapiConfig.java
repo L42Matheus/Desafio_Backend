14804103a053235761e6c4c8427473cdb4efcf11
matheus <lmatheus.tome@gmail.com>
2024-03-12 20:13:55 -0300

    Add RestTemplate configuration to provide a managed instance of `RestTemplate`.

---
+++ src/main/java/org/example/starwars/swapiConfig/swapiConfig.java
@@ -0,0 +1,16 @@
+package org.example.starwars.swapiConfig;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class swapiConfig {
+
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+
+}
