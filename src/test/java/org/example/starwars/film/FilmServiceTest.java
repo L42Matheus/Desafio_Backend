ed2dc1705c883d8aec08b4e424450690cf20c319
matheus <lmatheus.tome@gmail.com>
2024-03-12 21:40:22 -0300

    change unit test

--- src/test/java/org/example/starwars/film/FilmServiceTest.java
+++ src/test/java/org/example/starwars/film/FilmServiceTest.java
@@ -1,11 +1,16 @@
 package org.example.starwars.film;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
 class FilmServiceTest {
 
