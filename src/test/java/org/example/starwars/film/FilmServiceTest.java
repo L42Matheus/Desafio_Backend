82d2d36d1c1177dd767b509e79fe5971fc884bba
matheus <lmatheus.tome@gmail.com>
2024-03-12 21:31:41 -0300

    mocking the response from the service

--- src/test/java/org/example/starwars/film/FilmServiceTest.java
+++ src/test/java/org/example/starwars/film/FilmServiceTest.java
@@ -15,6 +15,24 @@ class FilmServiceTest {
     @Test
     void getAllFilms() {
 
+        Film film1 = new Film();
+        film1.setEpisodeId("4");
+
+        film1.setTitle("A New Hope");
+        film1.setDirector("George Lucas");
+
+        Film film2 = new Film();
+        film2.setEpisodeId("5");
+
+        film2.setTitle("The Empire Strikes Back");
+        film2.setDirector("Irvin Kershner");
+
+        List<Film> films = Arrays.asList(film1, film2);
+
+        when(filmService.getAllFilms()).thenReturn(films);
+
+        List<Film> result = filmController.getAllFilms();
+
 
 
     }
