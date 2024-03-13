249d8311eea1bc8ceaf7f87633f8929aea964011
matheus <lmatheus.tome@gmail.com>
2024-03-12 21:16:59 -0300

    add @mock to class filmService.

--- src/test/java/org/example/starwars/film/FilmServiceTest.java
+++ src/test/java/org/example/starwars/film/FilmServiceTest.java
@@ -1,13 +1,20 @@
 package org.example.starwars.film;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class FilmServiceTest {
 
+    @Mock
+    private FilmService filmService;
+
     @Test
     void getAllFilms() {
+
 
 
     }
