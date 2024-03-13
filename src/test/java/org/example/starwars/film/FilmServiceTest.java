5d3573b199bdae50b666898ebc06d9f90ae686f8
matheus <lmatheus.tome@gmail.com>
2024-03-12 21:15:51 -0300

    add unit test for getAllFilms.

---
+++ src/test/java/org/example/starwars/film/FilmServiceTest.java
@@ -0,0 +1,14 @@
+package org.example.starwars.film;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FilmServiceTest {
+
+    @Test
+    void getAllFilms() {
+
+
+    }
+}
