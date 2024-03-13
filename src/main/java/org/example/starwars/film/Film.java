5da556731486705216644687a78828f66ae04aba
matheus <lmatheus.tome@gmail.com>
2024-03-12 21:04:03 -0300

    add all atributes of swapi json

--- src/main/java/org/example/starwars/film/Film.java
+++ src/main/java/org/example/starwars/film/Film.java
@@ -3,6 +3,8 @@ package org.example.starwars.film;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class Film {
 
@@ -15,5 +17,13 @@ public class Film {
     private String releaseDate;
     @JsonProperty("opening_crawl")
     private String openingCrawl;
+    private List<String> characters;
+    private List<String> planets;
+    private List<String> starships;
+    private List<String> vehicles;
+    private List<String> species;
+    private String created;
+    private String edited;
+    private String url;
 
 }
